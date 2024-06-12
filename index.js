095fc608962aeca395c16e0923abdcf4dda4047e
sanmarhernandez <sandrahernandezaap@gmail.com>
2024-06-11 22:11:22 -0500

    Laboratorio6.1

--- index.js
+++ index.js
@@ -3,32 +3,46 @@ const express = require("express")//Importando la libreria
 const app = express() //inicializamos la variable de lalibreria
 const port = 3000 // Definimps el puerto a usar
 
+const mongoose =require('mongoose');// importa libreria mongoose
+
+// Obtengo la cadena de conexion del archivo .env
+require('dotenv').config()
+const DB_CONNECTION = process.env.DB_CONNECTION || ''
+mongoose.connect(DB_CONNECTION)//creo la cadena de conexin
+
+//Importamos las rutas del otro archivo
+app.use(express.urlencoded({extended: true}))//Acceder a la informacion de las urls
+app.use(express.json())//analizar informacion en formato JSON
+const UserRoutes = require('./routes/UserRoutes')
+app.use('/', UserRoutes)
+
+
 //Creando el servicio Web
 //Funcionalidad de nuestra api
 //[get, post, put, patch, delet]
 // res->Response -> Reespuesta
 //req .> Request -> Informacion de entrada
-app.get(`/`, (req, res) => {
+app.get('/', (req, res) => {
     // Muestra en pantalla Hello world
     res.send("Hello word")
 })
 
-app.get(`/saludar`, (req, res) => {
+app.get('/saludar', (req, res) => {
     res.send("Hola")
 })
 //servicio web con parametros
-app.get(`/saludar/:nombre`, (req, res) => {
+app.get('/saludar/:nombre', (req, res) => {
     //recibiendo parametro de la URL
     var nombre =req.params.nombre
     res.send("Hola " + nombre)
 })
-app.get(`/saludar/:nombre/:edad`, (req, res) => {
+app.get('/saludar/:nombre/:edad', (req, res) => {
     //recibiendo parametro de la URL
     var nombre =req.params.nombre
     var edad = req.params.edad
     res.send("Hola, me llamo  " + nombre + " y tengo " + edad) 
 })
-app.get(`/mascotas/:tipo`, (req, res) => {
+app.get('/mascotas/:tipo', (req, res) => {
     var tipo =req.params.tipo
     var animal = ""
     if(tipo == "perro"){
@@ -48,63 +62,63 @@ app.get(`/mascotas/:tipo`, (req, res) => {
 
 //API REST
 //Solicitud get
-app.get(`/usuario`, (req,res) => {
+/*app.get(/usuario', (req,res) => {
     res.send("Estoy consultando un usuario")
-})
+})*/
 
 //Solicitud por post
-app.post(`/usuario`, (req,res) => {
+/*app.post('/usuario', (req,res) => {
     res.send("Estoy creando un usuario")
 })
 //Solicitud por put
-app.put (`/usuario`, (req, res) => {
+app.put ('/usuario', (req, res) => {
     res.send("Estoy actualizando un ususario por PUT")//actualizando toda la informacion del usuario
 })
 //Solicitud por patch
-app.patch(`/usuario`, (req, res) =>{
+app.patch('/usuario', (req, res) =>{
     res.send("Estoy actualizando un usuario con PATCH")// actualizando un dato especifico del usuario
 })
 //Solicitud por delete
-app.delete(`/usuario`, (req, res) =>{
+app.delete('/usuario', (req, res) =>{
     res.send("Estoy eliminando un usuario")
 })
 //Ejecutamos en el servidor
-app.listen(por, () => {
+app.listen(port, () => {
     console.log("lisen on " + port)
 })
 
 
-app.get(`/despedirse/:nombre`, (req, res) => {
+app.get('/despedirse/:nombre', (req, res) => {
     var nombre =req.params.nombre
      res.send("Adios "  + nombre)
 
 })  
 
-app.get(`/despedirse/:nombre/edad`, (req, res) => {
+app.get('/despedirse/:nombre/edad', (req, res) => {
     var nombre =req.params.nombre
     var edad = req.params.edad
      res.send("Adios "  + nombre + "Espero estes muy bien")
 
 }) 
-app.get(`/despedirse`, (req, res) => {
+app.get('/despedirse', (req, res) => {
     res.send("Adios")
 
 })    
 
-app.get(`/Gato`, (req, res) => {
+app.get('/Gato', (req, res) => {
     res.send("Miau")
 
 })    
 
-app.get(`/Vaca`, (req, res) => {
+app.get('/Vaca', (req, res) => {
     res.send("Muuu")
 
 })    
 
-app.get(`/Perro`, (req, res) => {
+app.get('/Perro', (req, res) => {
     res.send("Guao")
 
-})    
+})*/
 
 
 // Ejecutamos el servidor
